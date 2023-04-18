c73d46946c8b2f099664789f7c40eb1e2495770a
Islam-Kaziev <kaziev19691@gmail.com>
2023-04-18 23:18:15 +0300

    added SelenideTest.java

--- src/test/java/com/github/SelenideTest.java
+++ src/test/java/com/github/SelenideTest.java
@@ -1,5 +1,17 @@
 package com.github;
 
-public class SelenideTest {
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideTest {
+    @Test
+        void checkingVisibleCode() {
+            open("https://github.com/selenide/selenide");
+            $("#wiki-tab").click();
+            $x("//a[text()='Soft assertions']").should(visible).click();
+            $("#user-content-3-using-junit5-extend-test-class").parent().shouldHave(text("Using JUnit5"));
+    }
 }
